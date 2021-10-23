a67ec6c113764ec2fb14091ff4ba8233ead92975
Marco David Chávez Chambi <83045424+MarcXZ18@users.noreply.github.com>
2021-10-22 21:19:30 -0500

    Codigo Ejercicio 01
    Para encontrar el mayor numero de un array

--- ADA_Lab_Sesion19-10/greatestNumber.java
+++ ADA_Lab_Sesion19-10/greatestNumber.java
@@ -1,1 +1,21 @@
-
+//Dado un array de numeros la funcion
+//calcula el mayor de estos
+public class greatestNumber {
+	
+	public static void main(String[] args) {
+		
+		int [] array = {1,2,3,4,5,6};
+		
+		System.out.println(numeroMayor(array));
+	}
+	
+	public static int numeroMayor(int[] array) {
+		int mayor = 0;
+		for (int i : array){
+			if (i > mayor) {
+				mayor = i;	
+			}
+		}
+		return mayor;		
+	}
+}
