542b49c9052a567346707d8e5b65bc30305bf3d4
Marco David Chávez Chambi <83045424+MarcXZ18@users.noreply.github.com>
2021-10-29 22:09:50 -0500

    Ejercicio 9 complejidad O(n^1/2)

---
+++ ADA_Lab_Sesion26-10/Ejer09.java
@@ -0,0 +1,6 @@
+	//Q9: Cual es la complejidad de
+	for (int i = 0; i * i < n; i++) {     //i^2>=n  i>n^1/2
+		System.out.println(i);              //k>n^1/2
+	}									                    //k=n^1/2	
+	
+	// Complejidad O(n^1/2)
