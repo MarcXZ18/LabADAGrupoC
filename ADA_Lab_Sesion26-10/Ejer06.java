dc46672fbcdd57d672fde46b11740e249c806f7a
Marco David Chávez Chambi <83045424+MarcXZ18@users.noreply.github.com>
2021-11-03 19:58:21 -0500

    Ejercicio 6 resubido
    Caso de prueba y breve explicacion de complejidad

--- ADA_Lab_Sesion26-10/Ejer06.java
+++ ADA_Lab_Sesion26-10/Ejer06.java
@@ -1,8 +1,22 @@
-	//Q6: Cual es la complejidad de
-	int p = 0; 						            //O(1)
-	for (int i = 1; p <= n; i++) { 		//p>n  k>n
-		p = p + i;						          //k(k+1)/2>n		
-	}                                 //k^2≅n
-										                //k=n^1/2
-	
-	// Complejidad O(n^1/2)
+
+public class Question6 {
+
+	public static void main(String[] args) {
+		ques6(16);
+	}
+
+	// funcion dada un entero n y un entero p
+	// creado internamente en la funcion imprimira
+	// numeros consecutivos desde el 1 que aumentaran
+	// su secuencia cada vez en 1
+	public static void ques6 (int n) {
+		//Q6: Cual es la complejidad de
+		int p = 0; 						       // O(1)
+		for (int i = 1; p <= n; i++) { 		   // p>n  k>n
+			p = p + i;						   // k(k+1)/2>n		
+			System.out.println(p);			   // k^2≅n
+		}                                      // k=n^1/2   										   
+		
+		// Complejidad O(n^1/2)		
+	}
+}
