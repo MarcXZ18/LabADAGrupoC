83dfc71df346ba83a42e1bc86b3f6a6a23b0b211
Marco David Chávez Chambi <83045424+MarcXZ18@users.noreply.github.com>
2021-12-05 22:07:16 -0500

    codigo area de cuadrado mas grande

---
+++ ADA_Lab_Sesión30-11/MaximalSquare.java
@@ -0,0 +1,35 @@
+
+public class MaximalSquare {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char [][] matrix = {{'1','0','1','0','0'},
+				{'1','0','1','1','1'},
+				{'1','1','1','1','1'},
+				{'1','0','0','1','0'}};
+		System.out.println(maximalSquare(matrix));
+	}
+	// Metodo dado un array doble de 0 y 1 calculara el area
+	// del cuadrado donde mas cantidad de 1 se encuentren
+	public static int maximalSquare(char[][]matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) 
+            return 0;        
+
+        int filas = matrix.length;
+        int cols = matrix[0].length;        
+        int[][] nMatrix = new int[filas + 1][cols + 1];
+        
+        int max = 0;
+        for (int i = 1; i <= filas; i++) {
+            for (int j = 1; j <= cols; j++) {
+                if (matrix[i - 1][j - 1] == '1') {
+                    nMatrix[i][j] = Math.min(nMatrix[i - 1][j - 1], Math.min(nMatrix[i - 1][j], nMatrix[i][j - 1])) + 1;
+                    max = Math.max(nMatrix[i][j], max);
+                }
+                else 
+                    nMatrix[i][j] = 0;	                
+            }
+        }
+        return max * max;
+	}
+}
