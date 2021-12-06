7bc3596caad882d960d8f50f51a4754b234f212e
Marco David Chávez Chambi <83045424+MarcXZ18@users.noreply.github.com>
2021-12-05 21:59:43 -0500

    codigo UniquePathII para encontrar caminos en una matriz

--- ADA_Lab_Sesión30-11/UniquePathII.java
+++ ADA_Lab_Sesión30-11/UniquePathII.java
@@ -1,1 +1,32 @@
+public class UniquePathII {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [][]obstacle = {{0,0,0},{0,1,0},{0,0,0}};
+		System.out.println(uniquePathsWithObstacles(obstacle));
+	}
+
+	public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid.length == 0) 
+            return 0;
+                 
+        int fila = obstacleGrid.length;
+        int col = obstacleGrid[0].length;
+         
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[fila-1][col-1] == 1) {
+            return 0;
+        }         
+        int[] path = new int[col];
+        path[0] = 1;
+         
+        for (int i = 0; i < fila; i++) {
+            for (int j = 0; j < col; j++) {
+                if (obstacleGrid[i][j] == 1) 
+                    path[j] = 0;
+                else if (j > 0) 
+                    path[j] = path[j] + path[j-1];                
+            }
+        }
+        return path[col - 1];    
+	}
+}
 
